de7c612fe1db654d6f35dcc8b9c354c4cee98dfa
anjahrot <157907978+anjahrot@users.noreply.github.com>
2024-06-05 21:30:26 +0200

    Edit removeFromArray.js to pass test spec

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-};
+ 
+//Use splice() instead of toSpliced() to overwrite the array
+const removeFromArray = function(anArray, ...objectsToRemove){
+    const normalArray = objectsToRemove;
+    for(let i=0;i<normalArray.length;i++){
+        for(let j=0;j<anArray.length;j++){
+          //Checks if array contains this value (indexOf returns -1 if it does not have it), 
+          //only if it does will it be removed (splice(-1,1) would delete last item which is not what we want...) 
+          if(anArray.indexOf(normalArray[i]) !== -1){
+            anArray.splice(anArray.indexOf(normalArray[i],j),1);
+       }
+     }
+    }
+    return anArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
