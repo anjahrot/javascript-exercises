48af1ed5be21bb7810495734efa7886622704045
anjahrot <157907978+anjahrot@users.noreply.github.com>
2024-06-06 08:58:27 +0200

    Edit tempConversion.js to pass test spec.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesF) {
+  return Math.round(5/9*(degreesF-32)*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesC) {
+  return Math.round((9/5*degreesC+32)*10)/10;
 };
 
 // Do not edit below this line
