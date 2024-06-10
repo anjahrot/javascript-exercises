af960f8b32a5a3c03b5e0ad4b451b32c5f3176ca
anjahrot <157907978+anjahrot@users.noreply.github.com>
2024-06-10 14:58:09 +0200

    Edit findTheOldest.js to pass test spec

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
+const getAge = function (birth, death) {
+    if(!death) {
+        death = new Date().getFullYear();
+     }
+    return death - birth;
+   };
 
-};
+const findTheOldest = function(array) {
+    const oldest = array.sort(function (a,b){
+         const lastAge = getAge(a.yearOfBirth, a.yearOfDeath);
+         const nextAge = getAge(b.yearOfBirth, b.yearOfDeath);
+         return lastAge > nextAge ? -1 : 1;
+     });
+    return oldest[0];
+    }
+    //return array.reduce((oldest,currentPerson) => {
+    //    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    //    const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+
+    //return oldestAge < currentAge ? currentPerson : oldest;
+    //});
+//};
 
 // Do not edit below this line
 module.exports = findTheOldest;
