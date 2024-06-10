d92e24c5a4ffcef573adeed3dcc87f834cd92f9f
anjahrot <157907978+anjahrot@users.noreply.github.com>
2024-06-10 12:17:24 +0200

    Edit palindrome.js to pass test in spec

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
-};
+const palindromes = function (str) {
+    const arr = Array.from(str.toLowerCase());
+    const filteredArr = arr.filter((item)=> /^[A-Za-z0-9]+$/.test(item));
+    const reverse = [...filteredArr].reverse();  //spreadoperator klonar arrayen før reverse()
+    console.log(reverse);
+    return filteredArr.join('') === reverse.join('');
+   };
 
 // Do not edit below this line
 module.exports = palindromes;
